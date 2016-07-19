82f009a8a556fc566d9d905e63880a65f03e087d
DevinFlick <DevinJFlick@gmail.com>
2016-07-19 19:52:42 -0400

    added status codes to get route

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var server = express();
+var $http = require('axios');
 
 var port = process.env.PORT || 8080;
+var apiKey = require('./config').apiKey;
+var baseURL = 'https://api.forecast.io/forecast/';
 
 //plugins middleware
 server.use(bodyParser.json());
@@ -11,9 +14,21 @@ server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 
 //test route
-server.get('/', function (request, response){
-  response.send('hello you beautiful son of a bitch!');
+server.get('/forecast/hourly/:lat,:lon', function (request, response){
+  $http.get(baseURL + apiKey + '/' + request.params.lat+','+request.params.lon)
+        .then(function(res){
+          var responseObj = {
+            latitude: res.data.latitude,
+            longitude: res.data.longitude,
+            hourly: res.data.hourly,
+          };
+          response.status(200).json(responseObj);
+        })
+        .catch(function(err){
+          console.log(err);
+          response.status(500).send({'baddddddddd'})
   });
+});
 
 //listen
 server.listen(port, function(){
