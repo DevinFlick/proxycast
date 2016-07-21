2e8059d4421e9ea347880c203323f78f1bca0cdd
DevinFlick <DevinJFlick@gmail.com>
2016-07-20 20:26:23 -0400

    added simple auth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var server = express();
 var $http = require('axios');
 var logger = require('./logger');
+var authorize = require('./auth');
 
 var port = process.env.PORT || 8080;
 var apiKey = require('./config').apiKey;
@@ -14,6 +15,8 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 server.use(logger);
+server.use(authorize);
+
 //test route
 server.get('/forecast/hourly/:lat,:lon', function (request, response){
   $http.get(baseURL + apiKey + '/' + request.params.lat+','+request.params.lon)
